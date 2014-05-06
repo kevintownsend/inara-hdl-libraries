911ba310a2499c782f38cc053c7c5c4625553944
testsubject42 <kt@krtownsend.com>
2014-05-05 21:03:41 -0500

    adding a basic fifo

---
+++ std_fifo/std_fifo.v
@@ -0,0 +1,46 @@
+module std_fifo(rst, clk, push, pop, d, q, full, empty, count, almost_empty, almost_full);
+    parameter WIDTH = 8;
+    parameter DEPTH = 6;
+    parameter DEPTH_ADDR_WIDTH = log2(DEPTH-1);
+    parameter ALMOST_EMPTY_COUNT = 1;
+    parameter ALMOST_FULL_COUNT = 1;
+    input rst;
+    input clk;
+    input push;
+    input pop;
+    input [WIDTH-1:0] d;
+    output [WIDTH-1:0] q;
+    output full;
+    output empty;
+    output [DEPTH_ADDR_WIDTH:0]count;
+    output almost_empty;
+    output almost_full;
+
+    reg [WIDTH-1:0] r_q;
+    reg [DEPTH_ADDR_WIDTH:0] r_end;
+    reg [DEPTH_ADDR_WIDTH:0] r_beg;
+
+    reg [WIDTH-1:0] ram [DEPTH-1:0];
+    always @(posedge clk) begin
+        if(rst) begin
+            r_end <= 0;
+            r_beg <= 0;
+        end else begin
+            r_q <= ram[r_end[DEPTH_ADDR_WIDTH-1:0]];
+            if(pop)
+                r_end <= r_end + 1;
+            if(push) begin
+                r_beg <= r_beg + 1;
+                ram[r_beg[DEPTH_ADDR_WIDTH-1:0]] <= d;
+            end
+        end
+    end
+    assign q = r_q;
+    assign empty = (r_end == r_beg);
+    assign full = (r_end[DEPTH_ADDR_WIDTH-1:0] == r_beg[DEPTH_ADDR_WIDTH-1:0]) && (r_end[DEPTH_ADDR_WIDTH] != r_beg[DEPTH_ADDR_WIDTH]);
+    assign count = r_beg - r_end;
+    assign almost_empty = (count < (1+ALMOST_EMPTY_COUNT));
+    assign almost_full = (count > (DEPTH-1-ALMOST_FULL_COUNT));
+
+    `include "log2.vh"
+endmodule
---
+++ std_fifo/std_fifo_tb.v
@@ -0,0 +1,89 @@
+`timescale 1ns/1ps
+module std_fifo_tb;
+
+reg rst, clk;
+reg push, pop;
+reg [7:0] d;
+wire [7:0] q;
+wire full, empty;
+wire [6:0] count;
+wire almost_empty;
+wire almost_full;
+
+std_fifo dut(
+    rst,
+    clk,
+    push,
+    pop,
+    d,
+    q,
+    full,
+    empty,
+    count,
+    almost_empty,
+    almost_full);
+integer i;
+
+initial begin
+    rst = 1;
+    push = 0; pop = 0;
+    d = 0;
+
+    #101 rst = 0;
+    #10 if(empty != 1) begin
+        $display("test1:failed");
+    end else begin
+        $display("test1:passed");
+    end
+    #10 push <=1;
+    d <= 1;
+    if(empty == 0)
+        $display("test2:failed");
+    if(full == 1)
+        $display("test2:failed full high");
+    for(i = 2; i < 64; i = i + 1) begin
+        #10 push <=1;
+        d <= i;
+        if(empty == 1) begin
+            $display("test3:failed empty high, round %d", i);
+        end
+        if(full == 1)
+            $display("test3:failed full high");
+    end
+    #10 push <= 1;
+    d <= 64;
+    if(empty == 1)
+        $display("error");
+    if(full == 1)
+        $display("error");
+    #10 if(full == 1)
+        $display("test4:passed");
+    else
+        $display("test4:error");
+    push <= 0;
+    pop <= 1;
+    for(i = 1; i < 65; i = i + 1) begin
+        #10 if(q != i) begin
+            $display("ERROR:");
+        end
+
+    end
+    pop <= 0;
+    #10 
+    if(empty != 1)
+        $display("ERROR:");
+    else
+        $display("test5:passed");
+    if(full != 0)
+        $display("ERROR:");
+    #100 $finish;
+
+    //TODO: check empty
+end
+
+initial begin
+    clk = 1;
+    forever #5 clk=~clk;
+end
+
+endmodule
