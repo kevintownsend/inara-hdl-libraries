85fe0f3c36160ac5e698511b458f9533facbb4d6
Kevin Townsend <kt@krtownsend.com>
2015-02-17 18:47:40 -0600

    completed omega network test

--- multistage_interconnect_network/basic_switch_ff.v
+++ multistage_interconnect_network/basic_switch_ff.v
@@ -1,5 +1,6 @@
-module basic_switch_ff(left_in, right_in, left_out, right_out, select);
+module basic_switch_ff(clk, left_in, right_in, left_out, right_out, select);
     parameter WIDTH = 64;
+    input clk;
     input [WIDTH-1:0] left_in, right_in;
     output reg [WIDTH-1:0] left_out, right_out;
     input select;
