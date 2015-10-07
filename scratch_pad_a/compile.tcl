c23983ab52ac472c9067f0009387ce67404ae1c9
Kevin Townsend <kt@krtownsend.com>
2015-10-07 12:12:14 -0500

    .

--- scratch_pad_a/compile.tcl
+++ scratch_pad_a/compile.tcl
@@ -8,6 +8,7 @@ read_verilog ../cross_bar/cross_bar.v
 read_verilog ../common/simple_ram.v
 #synth_design -no_iobuf -include_dirs ../. -top scratch_pad -part xc7v2000t
 synth_design -include_dirs ../. -top scratch_pad -part xc7v2000t
+create_clock -period 6.667 clk
 write_checkpoint -force $outputDir/post_synth.dcp
 report_timing_summary -file $outputDir/post_synth_timing_summary.rpt
 report_utilization -file $outputDir/post_synth_util.rpt
