2ca9bda80b6499d982a08290846779191669e427
kevintownsend <kt@krtownsend.com>
2015-10-07 16:49:56 -0500

    .

--- scratch_pad_a/compile.tcl
+++ scratch_pad_a/compile.tcl
@@ -8,11 +8,11 @@ read_verilog ../reorder_queue/reorder_queue.v
 read_verilog ../cross_bar/cross_bar.v
 read_verilog ../common/simple_ram.v
 #synth_design -no_iobuf -include_dirs ../. -top scratch_pad -part xc7v2000t
-synth_design -include_dirs ../. -top scratch_pad_synthesis -max_bram 12 -part xc7a200t
+synth_design -include_dirs ../. -top scratch_pad_synthesis -generic PORTS=32 -max_bram 100 -part xc7v2000t
 write_checkpoint -force $outputDir/post_synth.dcp
 report_timing_summary -file $outputDir/post_synth_timing_summary.rpt
 report_utilization -file $outputDir/post_synth_util.rpt
-opt_design
+#opt_design
 place_design
 route_design
 report_timing_summary -file $outputDir/post_route_timing_summary.rpt
